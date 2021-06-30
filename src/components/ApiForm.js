299d681e4896e0627ec891cf9af21208fd2559e2
Ron Wise <ron@ronwisephoto.com>
2021-06-29 20:36:54 -0400

    Updated code in ApiForm.js

--- src/components/ApiForm.js
+++ src/components/ApiForm.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import "./ApiForm.css" 
 
-const ApiForm = (props) => {
+const ApiForm = () => {
     const [choice, setChoice] = useState("")
     const [data, setData] = useState([])
 
@@ -15,23 +15,46 @@ const ApiForm = (props) => {
         }
     }, [choice])
 
-
     const [options, setOptions] = useState([])
     useEffect(()=>{
-        let arrayData
+        let arrayData;
         if (choice === "terrain"){
             let filteredData = data.filter(l => l.terrain != "TODO")
             let terrainNames = filteredData.map(l => l.terrain)
             let uniqTerrains = new Set(terrainNames)
             arrayData = Array.from(uniqTerrains)
-
         } else {
             arrayData = data.map(s => s.name)
         }
         setOptions(arrayData)
     }, [data])
 
+    const titleCase = (s) => {
+        let first = s[0].toUpperCase()
+        let remaining = s.slice(1)
+        return first + remaining
+    }
+
+    const [typeSelector, setTypeSelector] = useState("") 
+    const [displayData, setDisplayData] = useState([])
+    useEffect(() => {
+
+        let results;
+        if (choice == "species"){
+            results = data.filter(s => s.name == typeSelector)
+
+        } else {
+            results = data.filter(l => l.terrain == typeSelector)
+        }
+        // Handle Species Data  
+        console.log(results)
+        setDisplayData(results)
+        // Handle Terrain Data
+
+    }, [typeSelector])
+
     return (
+        <>
         <form>
             <label htmlFor="query">Pick an Api Query:</label>
             <select onChange={(e) => { setChoice(e.target.value) }} name="query" id="query">
@@ -40,31 +63,28 @@ const ApiForm = (props) => {
                 <option value="terrain">Terrain</option>
             </select>
             <br />
-            {choice == "terrain"
-                ? <div>
-                    <label htmlFor="terrain">Terrain:</label>
-                    <select name="terrain" id="terrain">
+            {choice && 
+                <div>
+                    <label htmlFor={choice}>{titleCase(choice)}:</label>
+                    <select onChange={(e) => setTypeSelector(e.target.value)} name={choice} id={choice}>
                         <option value="" selected disabled hidden></option>
                         {options?.map((o) => {
                             return (
-                                <option value={o}>{o}</option>
-                            )
-                        })}
-                    </select>
-                </div>
-                : <div>
-                    <label htmlFor="species">Species:</label>
-                    <select name="species" id="species">
-                        <option value="" selected disabled hidden></option>
-                        {options?.map((o) => {
-                            return (
-                                <option onChange={o}>{o}</option>
+                                <option key={o} value={o}>{o}</option>
                             )
                         })}
                     </select>
                 </div>
             }
         </form>
+        <ul>
+            {displayData?.map((elm) => {
+                return (
+                    <li>{elm.name}</li>
+                )
+            })}
+        </ul>
+        </>
     );
 }
 
